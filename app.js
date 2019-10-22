369881229780bca4be9a9777a89df47305ccad6c
venish-vk7 <venishvk7@gmail.com>
2019-10-22 23:26:46 +0530

    added TaskManager

--- app.js
+++ app.js
@@ -5,17 +5,33 @@
  */
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var app = express();
 
 // Configuring the static file here
 app.use(express.static('public'))
 
-// Handling the request items here
-app.get('/view' , (req ,res) => {
- res.sendFile(path.join(__dirname + '/public/view.html'))
+app.get('/404' , (req ,res) => {
+    res.sendFile(
+        path.join(__dirname + `/public/404.html`)
+    );
 })
 
-app.get('*' , (req , res) => {
+// Handling the request items here
+app.get('/*', (req, res) => {
+    var root = path.join(__dirname + `/public/${req.path.substring(1 , req.path.length)}.html`)
+    if(fs.existsSync(root)){
+        res.sendFile(root);
+    }else{
+        res.redirect('/404')
+    }
+})
+
+
+
+
+
+app.get('*', (req, res) => {
     res.redirect('/')
 })
 
